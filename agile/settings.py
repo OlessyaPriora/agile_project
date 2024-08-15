8d1dd66f905c81dca06ca892419c2b0bdfe9c986
OlessyaPriora <Olessya_29_03_91@mail.ru>
2024-08-15 12:06:04 +0300

    Third commit

--- agile/settings.py
+++ agile/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.tasks.apps.TasksConfig',
+    'apps.projects.apps.ProjectsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
