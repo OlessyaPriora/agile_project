e1c7f99d4d9651d713998177ce8e93045fc6e68a
OlessyaPriora <Olessya_29_03_91@mail.ru>
2024-08-16 09:54:31 +0300

    Fifth commit Task 8

--- apps/projects/serializers/project_serializers.py
+++ apps/projects/serializers/project_serializers.py
@@ -22,3 +22,12 @@ class CreateProjectSerializer(serializers.ModelSerializer):
             )
 
         return value
+
+
+class ProjectDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'description', 'count_of_files')
+
+
